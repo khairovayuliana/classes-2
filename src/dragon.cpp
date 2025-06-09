1f23093cbf4ceda0ff84604154a864d0ef184d90
khairovayuliana <y.khayrova@inbox.ru>
2025-06-09 15:41:40 +0300

    Update dragon.cpp

--- src/dragon.cpp
+++ src/dragon.cpp
@@ -1,6 +1,21 @@
 
 #include "dragon.h"
 
+// геттеры
+std::string Dragon::getName() const { return name; }
+int Dragon::getTailLength() const { return tail_length; }
+int Dragon::getAge() const { return age; }
+int Dragon::getMagicPower() const { return magic_power; }
+bool Dragon::isRegistered() const { return is_registered; }
+Dragon::Type Dragon::getType() const { return type; }
+
+Dragon::AgeCategory Dragon::getAgeCategory() const {
+    if (age <= 5) return AgeCategory::BABY;
+    if (age <= 20) return AgeCategory::YOUNG;
+    if (age <= 100) return AgeCategory::ADULT;
+    return AgeCategory::ANCIENT;
+}
+
 void Dragon::registerDragon() {
     // your code here
 }
